0e468b2b60959edb0ea45ab122a42181fb4b7e5b
Sandro-Yu <yu.sandro@gmail.com>
2016-02-25 01:06:18 -0800

    able to view database
    only sees database at startup, new additions do not show up

--- FridgeV1/app/src/main/java/com/example/mowolfvillageon/fridgev1/Main2Activity.java
+++ FridgeV1/app/src/main/java/com/example/mowolfvillageon/fridgev1/Main2Activity.java
@@ -6,15 +6,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
+
+    protected ArrayList<String> foodNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +36,33 @@ public class Main2Activity extends AppCompatActivity {
 
     private void populateListView() {
         // Create Fridge List of items
-        String[] starterFoods = {"Apple", "Chicken", "Water"};
+        //String[] starterFoods = {"Apple", "Chicken", "Water"};
 
         // Set up adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this,
-                R.layout.textlistview, // layout being created
-                starterFoods); // items to add to list
+        Firebase myFirebaseRef = new Firebase("https://fridge-it2.firebaseio.com/");
 
-        // Configure List View
-        ListView list = (ListView) findViewById(R.id.listViewFridge);
-        list.setAdapter(adapter);
+        myFirebaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    String foodName = (String) child.child("name").getValue();
+                    foodNames.add(foodName);
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                        Main2Activity.this,
+                        R.layout.textlistview, // layout being created
+                        foodNames); //items to add to list
+
+                ListView list = (ListView) findViewById(R.id.listViewFridge);
+                list.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+
+
     }
 
     // Button for inserting an Item

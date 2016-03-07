f2b7ccbdf685738f7630151ea52a481f59537277
roger <roger.ruan8@gmail.com>
2016-03-07 12:18:28 -0800

    imports implemented for onView()

--- FridgeV1/app/src/androidTest/java/com/example/mowolfvillageon/fridgev1/ExpressoTest.java
+++ FridgeV1/app/src/androidTest/java/com/example/mowolfvillageon/fridgev1/ExpressoTest.java
@@ -8,6 +8,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * Created by mowolfvillageon on 3/7/16.
  */
@@ -19,17 +26,17 @@ public class ExpressoTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
-    /* @Test
+     @Test
     public void listGoesOverTheFold() {
         onView(withText("Hello world!")).check(matches(isDisplayed()));
-    } */
+    }
 
     @Test
     public void removedItem() {
         //onView();
-        /*onView(withId(R.id.my_view))            // withId(R.id.my_view) is a ViewMatcher
-                .perform(click())               // click() is a ViewAction
-                .check(matches(isDisplayed())); // matches(isDisplayed()) is a ViewAssertion */
+        //onView(withId(R.id.my_view))            // withId(R.id.my_view) is a ViewMatcher
+                //.perform(click())               // click() is a ViewAction
+                //.check(matches(isDisplayed())); // matches(isDisplayed()) is a ViewAssertion
     }
 }
 

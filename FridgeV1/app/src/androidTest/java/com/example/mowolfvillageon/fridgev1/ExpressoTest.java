7b7b405025583b0c898cab62114aec3f000b5244
Stephanie Villalobos <stephanievlobos@gmail.com>
2016-03-07 12:19:31 -0800

    ..

--- FridgeV1/app/src/androidTest/java/com/example/mowolfvillageon/fridgev1/ExpressoTest.java
+++ FridgeV1/app/src/androidTest/java/com/example/mowolfvillageon/fridgev1/ExpressoTest.java
@@ -4,16 +4,20 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+<<<<<<< Updated upstream
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+=======
+>>>>>>> Stashed changes
 
 /**
  * Created by mowolfvillageon on 3/7/16.
@@ -22,17 +26,27 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ExpressoTest {
-
+    String sFood;
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
      @Test
     public void listGoesOverTheFold() {
         onView(withText("Hello world!")).check(matches(isDisplayed()));
+<<<<<<< Updated upstream
     }
 
+=======
+    } */
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        sFood = "Apple";
+    }
+>>>>>>> Stashed changes
     @Test
     public void removedItem() {
+        //onView(withId(R.id.goodbye_button)).perform(click());
         //onView();
         //onView(withId(R.id.my_view))            // withId(R.id.my_view) is a ViewMatcher
                 //.perform(click())               // click() is a ViewAction

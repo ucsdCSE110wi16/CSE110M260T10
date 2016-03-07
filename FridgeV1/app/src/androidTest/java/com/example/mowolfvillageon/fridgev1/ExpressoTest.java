b580b0514e69090fbaccbc40aea76bd8bf714717
Stephanie Villalobos <stephanievlobos@gmail.com>
2016-03-07 13:18:02 -0800

    Design updates

--- FridgeV1/app/src/androidTest/java/com/example/mowolfvillageon/fridgev1/ExpressoTest.java
+++ FridgeV1/app/src/androidTest/java/com/example/mowolfvillageon/fridgev1/ExpressoTest.java
@@ -3,21 +3,24 @@ package com.example.mowolfvillageon.fridgev1;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.widget.ArrayAdapter;
+
+import com.firebase.client.Firebase;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-<<<<<<< Updated upstream
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-=======
->>>>>>> Stashed changes
+
 
 /**
  * Created by mowolfvillageon on 3/7/16.
@@ -30,27 +33,28 @@ public class ExpressoTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
-     @Test
+    /* @Test
     public void listGoesOverTheFold() {
         onView(withText("Hello world!")).check(matches(isDisplayed()));
-<<<<<<< Updated upstream
-    }
-
-=======
     } */
     @Before
     public void initValidString() {
         // Specify a valid string.
         sFood = "Apple";
     }
->>>>>>> Stashed changes
     @Test
     public void removedItem() {
+        Firebase myFirebaseRef = new Firebase("https://fridge-it2.firebaseio.com/");
+
+
+        //String value = (String)adapter.getItemAtPosition(position);
+        //assertThat(removedItem(););
+        //onView(withId(R.id.listViewFridgeRemove)).perform(click()).check();
         //onView(withId(R.id.goodbye_button)).perform(click());
         //onView();
-        //onView(withId(R.id.my_view))            // withId(R.id.my_view) is a ViewMatcher
-                //.perform(click())               // click() is a ViewAction
-                //.check(matches(isDisplayed())); // matches(isDisplayed()) is a ViewAssertion
+        /*onView(withId(R.id.my_view))            // withId(R.id.my_view) is a ViewMatcher
+                .perform(click())               // click() is a ViewAction
+                .check(matches(isDisplayed())); // matches(isDisplayed()) is a ViewAssertion */
     }
 }
 
